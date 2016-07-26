b2a8b77d7afbc119fbf69cb9e2f3d92c30b1b260
Rei Higuchi <rei.higuchi.2001@gmail.com>
2016-07-26 20:09:16 +0900

    solve the problem that this function cannot read obj file that doesn't have normal data.

--- FPS/util/objloader.cpp
+++ FPS/util/objloader.cpp
@@ -25,11 +25,12 @@ loadOBJ::loadOBJ(const char * path){
 	std::vector<glm::vec3> temp_vertices; 
 	std::vector<glm::vec2> temp_uvs;
     std::vector<glm::vec3> temp_normals;
+    bool hasNormal = false;
 
 
 	FILE * file = fopen(path, "r");
 	if( file == NULL ){
-		printf("Impossible to open the file ! Are you in the right path ? See Tutorial 1 for details\n");
+        printf("Impossible to open the file ! Are you in the right path ?\n");
 		getchar();
 	}
 
@@ -53,24 +54,39 @@ loadOBJ::loadOBJ(const char * path){
 			uv.y = -uv.y; // Invert V coordinate since we will only use DDS texture, which are inverted. Remove if you want to use TGA or BMP loaders.
 			temp_uvs.push_back(uv);
 		}else if ( strcmp( lineHeader, "vn" ) == 0 ){
+            hasNormal = true;
 			glm::vec3 normal;
 			fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z );
 			temp_normals.push_back(normal);
 		}else if ( strcmp( lineHeader, "f" ) == 0 ){
-			unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
-			int matches = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n", &vertexIndex[0], &uvIndex[0], &normalIndex[0], &vertexIndex[1], &uvIndex[1], &normalIndex[1], &vertexIndex[2], &uvIndex[2], &normalIndex[2] );
-			if (matches != 9){
-				printf("File can't be read by our simple parser :-( Try exporting with other options\n");
-			}
-			vertexIndices.push_back(vertexIndex[0]);
-			vertexIndices.push_back(vertexIndex[1]);
-			vertexIndices.push_back(vertexIndex[2]);
-			uvIndices    .push_back(uvIndex[0]);
-			uvIndices    .push_back(uvIndex[1]);
-			uvIndices    .push_back(uvIndex[2]);
-			normalIndices.push_back(normalIndex[0]);
-			normalIndices.push_back(normalIndex[1]);
-			normalIndices.push_back(normalIndex[2]);
+            if(hasNormal){
+                unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
+                int matches = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n", &vertexIndex[0], &uvIndex[0], &normalIndex[0], &vertexIndex[1], &uvIndex[1], &normalIndex[1], &vertexIndex[2], &uvIndex[2], &normalIndex[2] );
+                if (matches != 9){
+                    printf("File can't be read by our simple parser :-( Try exporting with other options\n");
+                }
+                vertexIndices.push_back(vertexIndex[0]);
+                vertexIndices.push_back(vertexIndex[1]);
+                vertexIndices.push_back(vertexIndex[2]);
+                uvIndices    .push_back(uvIndex[0]);
+                uvIndices    .push_back(uvIndex[1]);
+                uvIndices    .push_back(uvIndex[2]);
+                normalIndices.push_back(normalIndex[0]);
+                normalIndices.push_back(normalIndex[1]);
+                normalIndices.push_back(normalIndex[2]);
+            }else{
+                unsigned int vertexIndex[3], uvIndex[3];
+                int matches = fscanf(file, "%d/%d %d/%d %d/%d\n", &vertexIndex[0], &uvIndex[0], &vertexIndex[1], &uvIndex[1], &vertexIndex[2], &uvIndex[2] );
+                if(matches != 6){
+                    printf("File can't be read by our simple parser :-( Try exporting with other options\n");
+                }
+                vertexIndices.push_back(vertexIndex[0]);
+                vertexIndices.push_back(vertexIndex[1]);
+                vertexIndices.push_back(vertexIndex[2]);
+                uvIndices    .push_back(uvIndex[0]);
+                uvIndices    .push_back(uvIndex[1]);
+                uvIndices    .push_back(uvIndex[2]);
+            }
 		}else{
 			// Probably a comment, eat up the rest of the line
 			char stupidBuffer[1000];
@@ -80,31 +96,39 @@ loadOBJ::loadOBJ(const char * path){
 	}
 
 	// For each vertex of each triangle
-	for( unsigned int i=0; i<vertexIndices.size(); i++ ){
+    if(hasNormal){
+        for( unsigned int i=0; i<vertexIndices.size(); i++ ){
 
-        // Get the indices of its attributes
-		unsigned int vertexIndex = vertexIndices[i];
-		unsigned int uvIndex = uvIndices[i];
-		unsigned int normalIndex = normalIndices[i];
-		
-		// Get the attributes thanks to the index
-		glm::vec3 vertex = temp_vertices[ vertexIndex-1 ];
-		glm::vec2 uv = temp_uvs[ uvIndex-1 ];
-		glm::vec3 normal = temp_normals[ normalIndex-1 ];
-		
-		// Put the attributes in buffers
-        vertices_.push_back(vertex);
-        uvs_     .push_back(uv);
-        normals_ .push_back(normal);
-	
-	}
-    if(normals_.size() == 0){
-        indexVBO(vertices_,uvs_,indices_,indexedVertices_,indexedUvs_);
-    }else{
+            // Get the indices of its attributes
+            unsigned int vertexIndex = vertexIndices[i];
+            unsigned int uvIndex = uvIndices[i];
+            unsigned int normalIndex = normalIndices[i];
+
+            // Get the attributes thanks to the index
+            glm::vec3 vertex = temp_vertices[ vertexIndex-1 ];
+            glm::vec2 uv = temp_uvs[ uvIndex-1 ];
+            glm::vec3 normal = temp_normals[ normalIndex-1 ];
+
+            // Put the attributes in buffers
+            vertices_.push_back(vertex);
+            uvs_     .push_back(uv);
+            normals_ .push_back(normal);
+
+        }
         indexVBO(vertices_,uvs_,normals_,indices_,indexedVertices_,indexedUvs_,indexedNormals_);
+    }else{
+        for( unsigned int i = 0 ; i < vertexIndices.size(); i++){
+            unsigned int vertexIndex = vertexIndices[i];
+            unsigned int uvIndex = uvIndices[i];
+
+            glm::vec3 vertex = temp_vertices[ vertexIndex-1 ];
+            glm::vec2 uv = temp_uvs[ uvIndex-1 ];
+
+            vertices_.push_back(vertex);
+            uvs_.push_back(uv);
+        }
+        indexVBO(vertices_,uvs_,indices_,indexedVertices_,indexedUvs_);
     }
-
-
 }
 
 
