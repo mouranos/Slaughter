1b67c7b3f647ce52f3fd293d4985b6e1822a7c0f
Rei Higuchi <rei.higuchi.2001@gmail.com>
2016-07-28 22:02:08 +0900

    a

--- FPS/main/title.cpp
+++ FPS/main/title.cpp
@@ -8,6 +8,7 @@
 #include <glm/gtx/transform.hpp>
 #include <iostream>
 #include "title.h"
+#include <math.h>
 
 constexpr unsigned int WIDTH = 1440;
 constexpr unsigned int HEIGHT = 900;
@@ -25,18 +26,24 @@ bool title()
     glDepthFunc(GL_LESS);
 
     RenderText text(U"Enterを押してください",TEXT_SIZE);
-
+    double lastTime = glfwGetTime();
+    double x = 0, y = 0, angle = 0;
     do
     {
+        double currentTime = glfwGetTime();
+        double deltaTime = currentTime - lastTime;
+        lastTime = currentTime;
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        text.render(300,300);
-
+        x = y += 300 * deltaTime;
+        angle += 360.0/2.0 * deltaTime;
+        if(angle > 360){
+            angle -= 360;
+        }
+        text.render(x + 1000 * sin(angle * M_PI/180.0),y + 500 * cos(angle * M_PI/180.0));
         glfwSwapBuffers(window);
         glfwPollEvents();
     } while(glfwGetKey(window, GLFW_KEY_ENTER) != GLFW_PRESS &&
             glfwWindowShouldClose(window) == 0);
 
-    glfwTerminate();
     return true;
 }
