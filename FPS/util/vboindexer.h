2551b18ededca5430a10ec66f4a3823c05eadf8f
Rei Higuchi <rei.higuchi.2001@gmail.com>
2016-08-11 14:41:05 +0900

    same with previous commit.

--- FPS/util/vboindexer.h
+++ FPS/util/vboindexer.h
@@ -25,6 +25,14 @@ void indexVBO(
     std::vector<glm::vec2> &out_uvs
 );
 
+void indexVBO(
+    std::vector<glm::vec3> &in_vertices,
+    std::vector<glm::vec3> &in_uvs,
+    std::vector<unsigned short> &out_indices,
+    std::vector<glm::vec3> &out_vertices,
+    std::vector<glm::vec3> &out_uvs
+);
+
 void indexVBO_TBN(
 	std::vector<glm::vec3> & in_vertices,
 	std::vector<glm::vec2> & in_uvs,
