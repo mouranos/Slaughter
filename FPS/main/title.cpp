5ed86595db0b76ae7d2f72df1741a3ddc29664e0
Rei Higuchi <rei.higuchi.2001@gmail.com>
2016-08-11 14:38:13 +0900

    change back ground color and animation.

--- FPS/main/title.cpp
+++ FPS/main/title.cpp
@@ -21,11 +21,12 @@ bool title()
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     glfwPollEvents();
-    glClearColor(0.21f, 0.24f, 0.29f, 1.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.f);
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
 
-    RenderText text(U"Enterを押してください",TEXT_SIZE);
+    RenderText instructionToStart(U"Enterを押してゲームを開始！",TEXT_SIZE);
+    RenderText manipulation(U"移動は矢印キー、ジャンプはスペースキー",TEXT_SIZE);
     double lastTime = glfwGetTime();
     double x = 0, y = 0, angle = 0;
     do
@@ -34,16 +35,44 @@ bool title()
         double deltaTime = currentTime - lastTime;
         lastTime = currentTime;
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        x = y += 300 * deltaTime;
-        angle += 360.0/2.0 * deltaTime;
+//        angle += 360.0/3.0 * deltaTime;
         if(angle > 360){
             angle -= 360;
         }
-        text.render(x + 1000 * sin(angle * M_PI/180.0),y + 500 * cos(angle * M_PI/180.0));
+
+//        instructionToStart.render(x + 1000 * sin(angle *M_PI/180.0),y + 500 * cos(angle * M_PI/180.0));
+
+        bool isUnsignedx,isUnsignedy;
+        if(0 > x){
+            isUnsignedx  = true;
+        }
+        else if(x > 1440 * 2){
+            isUnsignedx = false;
+        }
+        if(0 > y){
+            isUnsignedy = true;
+        }
+        else if(y > 832 * 2){
+            isUnsignedy = false;
+        }
+        if(isUnsignedx){
+            x += 500 * deltaTime;
+        }else{
+            x -= 500 * deltaTime;
+        }
+        if(isUnsignedy){
+            y += 500 * deltaTime;
+        }else{
+            y -= 500 * deltaTime;
+        }
+        instructionToStart.render(x,y);
+        manipulation.render(x,y-100);
         glfwSwapBuffers(window);
         glfwPollEvents();
     } while(glfwGetKey(window, GLFW_KEY_ENTER) != GLFW_PRESS &&
             glfwWindowShouldClose(window) == 0);
+    int width,height;
+    glfwGetWindowSize(window,&width,&height);
 
     return true;
 }
